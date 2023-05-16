a1d8cf147c0f066843a00519008f2bd129380490
Ryu jisu <wltn1418@gmail.com>
2023-05-16 10:50:47 +0900

    showall

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,9 +1,18 @@
+import { useSelector } from "react-redux";
 import Filter from "../components/Filter";
+import ProductInstance from "../components/ProductInstance";
 
 const Product = () => {
+  const products = useSelector((state) => state.product.product);
   return (
     <>
       <Filter />
+      <ul className="grid grid-cols-4 gap-[1.5rem]">
+        {products &&
+          products.map((product) => (
+            <ProductInstance key={product.id} info={product} />
+          ))}
+      </ul>
     </>
   );
 };
