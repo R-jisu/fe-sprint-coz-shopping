d091f031597f0038a05eafdc796099b37bb98c95
Ryu jisu <wltn1418@gmail.com>
2023-05-16 11:30:17 +0900

    filter

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,19 +1,30 @@
-import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import Filter from "../components/Filter";
 import ProductInstance from "../components/ProductInstance";
 
 const Product = () => {
+  const [filteredData, setFilteredData] = useState([]);
   const type = useSelector((state) => state.filter.type);
-  console.log("productpage: ", type);
   const products = useSelector((state) => state.product.product);
   //filter type에 따라서 prodcuts filter진행
   //해당 데이터 렌더링
+  useEffect(() => {
+    if (products.length !== 0) {
+      if (type === "All") setFilteredData(products);
+      else {
+        const filterArr = products.filter((el) => el.type === type);
+        setFilteredData(filterArr);
+      }
+    }
+  }, [products, type]);
+
   return (
     <div className="flex flex-col items-center mx-[4.75rem]">
       <Filter />
       <ul className="flex flex-wrap gap-[1.5rem] justify-center">
-        {products &&
-          products.map((product) => (
+        {filteredData &&
+          filteredData.map((product) => (
             <ProductInstance key={product.id} info={product} />
           ))}
       </ul>
